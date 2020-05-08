bb82c5f6684ffe93dd73c57b487faba38cf29b1b
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-05-07 22:15:59 -0600

    made a basic component in wishlist.js

---
+++ src/components/wishlist.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { connect } from "react-redux";
+import { getList } from '../redux/actions/WishlistActions';
+
+const FamilyMembers = ({gifts, getList, match}) =>{
+
+    useEffect(()=>{
+        getList(match.params.id);
+    },[]);
+
+    return(
+        <div className="Wishlist">
+            <h2>Wishlist</h2>
+            {gifts.map((gift)=>(
+                <div className="gift">
+                    <h3>{gift.name}</h3>
+                    <p>{gift.gift_url}</p>
+                    <p>{gift.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+      gifts:state.wishlistReducer.gifts
+    };
+  }
+
+export default connect(mapStateToProps, { getList })(FamilyMembers);
