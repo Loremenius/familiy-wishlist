3c73408ae1b04ecad250be3a51c77ec18be133f3
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-05-11 13:58:17 -0600

    removed link import in Wishlist.js

--- src/components/wishlist.js
+++ src/components/wishlist.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react"
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getList } from '../redux/actions/WishlistActions';
 
