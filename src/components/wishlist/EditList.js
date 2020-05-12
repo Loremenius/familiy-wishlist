b6585db47cab1c7bbd799dc75cd20b0d429b2003
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-05-11 20:43:13 -0600

    Created new file EditList.js

---
+++ src/components/wishlist/EditList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const EditList = ({gifts}) =>{
+
+    return(
+        <div className="Wishlist">
+            <h2>Wishlist</h2>
+            {gifts.map((gift)=>(
+                <div className="gift">
+                    <h3>{gift.name}</h3>
+                    <p>{gift.gift_url}</p>
+                    <p>{gift.description}</p>
+                    <button>Edit Gift</button>
+                    <button>Remove Gift</button>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+      gifts:state.wishlistReducer.gifts
+    };
+  }
+
+export default connect(mapStateToProps, { })(EditList);
