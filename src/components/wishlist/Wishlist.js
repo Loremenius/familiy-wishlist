dc50ecd456c65aace72719c71bdb722afd8a97a0
Marcellino Fernandez <munch2000@gmail.com>
2020-05-13 16:19:55 -0600

    changed wishlist to use the edit and view list files

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getList } from '../../redux/actions/WishlistActions';
+import { EditList } from './EditList';
+import { ViewList } from './ViewList';
 
-const Wishlist = ({gifts, getList, match}) =>{
-
+const Wishlist = ({gifts, getList, match, user_id}) =>{
+    const displayList = ()=>{
+        if (match.params.id === user_id) return <EditList gifts = {gifts}/>
+        else return <ViewList gifts = {gifts}/> 
+    }
     useEffect(()=>{
         getList(match.params.id);
     },[]);
@@ -11,20 +16,15 @@ const Wishlist = ({gifts, getList, match}) =>{
     return(
         <div className="Wishlist">
             <h2>Wishlist</h2>
-            {gifts.map((gift)=>(
-                <div className="gift">
-                    <h3>{gift.name}</h3>
-                    <p>{gift.gift_url}</p>
-                    <p>{gift.description}</p>
-                </div>
-            ))}
+            {displayList()}
         </div>
     )
 }
 
 function mapStateToProps(state) {
     return {
-      gifts:state.wishlistReducer.gifts
+      gifts:state.wishlistReducer.gifts,
+      user_id:state.loginReducer.user.user_id
     };
   }
 
