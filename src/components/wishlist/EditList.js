f7adc4806cb5c9197d6416bf674c53b4c8ccab91
Marcellino Fernandez <munch2000@gmail.com>
2020-05-14 23:18:32 -0600

    EditList adds a key for each gift

--- src/components/wishlist/EditList.js
+++ src/components/wishlist/EditList.js
@@ -7,7 +7,7 @@ const EditList = ({gifts}) =>{
         <div className="Wishlist">
             <h2>Wishlist</h2>
             {gifts.map((gift)=>(
-                <div className="gift">
+                <div className="gift" key={gift.id}>
                     <h3>{gift.name}</h3>
                     <p>{gift.gift_url}</p>
                     <p>{gift.description}</p>
