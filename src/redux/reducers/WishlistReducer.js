d4424e088f4aa445de6494b50b564aec6db5ddf5
Marcellino Fernandez <munch2000@gmail.com>
2020-05-22 22:46:10 -0600

    added function and case in Wishlist reducer to update array of gifts after successful edit

--- src/redux/reducers/WishlistReducer.js
+++ src/redux/reducers/WishlistReducer.js
@@ -2,8 +2,20 @@
 import {
     GET_FAMILIES,
     GET_MEMBERS,
-    GET_LIST
+    GET_LIST,
+    EDIT_GIFT
     } from "../actions/WishlistActions";
+
+import findGiftIndex from "./functions/findGiftIndex";
+
+function editGift(arr, gift){
+    // use findGiftIndex function to get the index of the gift
+    index = findGiftIndex(arr, gift);
+    // update gift
+    arr[index] = gift
+    //return updated array
+    return arr
+}
 
 export const reducer = (state = initialState, action) => {
     switch(action.type){
@@ -21,6 +33,11 @@ export const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 gifts: action.payload,
+            }
+        case EDIT_GIFT:
+            return {
+                ...state,
+                gifts: editGift(state.gifts, action.payload)
             }
         default:
             return state;
