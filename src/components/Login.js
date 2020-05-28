0d4237c7805d2b2dcd9b213b12fc4b9b0169c8c6
Marcellino Fernandez <munch2000@gmail.com>
2020-05-28 12:39:27 -0600

    added functionality for login submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import { connect } from "react-redux";
-import { loginUser } from '../redux/actions/LoginRegisterActions';
+import { loginUser, setError } from '../redux/actions/LoginRegisterActions';
 import { Link } from "react-router-dom";
 
-const Login = ({ loginUser, history }) =>{
+const Login = ({ loginUser, history, error, setError }) =>{
     const [user, setUser] = useState({username:"", password:""});
     const onChange = (e) =>{
         setUser({
@@ -14,7 +14,23 @@ const Login = ({ loginUser, history }) =>{
 
     const onSubmit = (e) =>{
         e.preventDefault();
-        loginUser(user, history);
+        // set error to empty string
+        setError('');
+        // check to make sure all fields are filled out
+        if(!!user.username && !!user.password){
+                // call action to send data to back-end.
+                loginUser(user, history);
+        // if not all fields are filled out. change error to display it. 
+        }else{
+            setError('All fields are required');
+        }
+    }
+
+    const displayError = () =>{
+        // if the error is not an empty string: create a paragraph to disaply it.
+        if(!!error) return (<p className='error'>{error}</p>)
+        // if error is a empty string: create empty paragraph.
+        else return (<p></p>)
     }
 
     return (
@@ -42,6 +58,7 @@ const Login = ({ loginUser, history }) =>{
                 </label>
                 <button> Login </button>
             </form>
+            {displayError()}
             <Link to='/register'>Register new user</Link>
         </div>
     )
@@ -54,4 +71,4 @@ function mapStateToProps(state) {
     };
   }
 
-export default connect(mapStateToProps, { loginUser })(Login);
+export default connect(mapStateToProps, { loginUser, setError })(Login);
