fd7f8ad32ba591a53d88360b11bf509edd38bbe4
Marcellino Fernandez <munch2000@gmail.com>
2020-06-16 17:59:21 -0600

    added function to EditList to fix url on gifts and display them if there is an url.

--- src/components/wishlist/EditList.js
+++ src/components/wishlist/EditList.js
@@ -3,6 +3,16 @@ import { connect } from "react-redux";
 
 const EditList = ({ gifts, history, user_id, match }) =>{
 
+    function createLinkToGift(gift_url){
+        // get the first 4 characters of the url
+        const urlBegin = gift_url.slice(0,4);
+        //if the url does not include http: add http to the url
+        if (urlBegin !== 'http' && !!urlBegin) gift_url = `http://${gift_url}`
+        //if there is a url, display a link with reference to that url. Otherwise display empty paragraph tags
+        if (!!gift_url) return(<a href={gift_url} target="_blank" >View gift at original website</a>)
+        else return (<p></p>)
+    }
+
     return(
         <div className = 'editList'>
             <button className="add" onClick={()=>history.push(`/wishlist/${match.params.family}/${user_id}/add`)}>Add gift to wishlist</button>
@@ -13,7 +23,9 @@ const EditList = ({ gifts, history, user_id, match }) =>{
                 {gifts.map((gift)=>(
                     <div className="gift" key={gift.id} onClick={()=>history.push(`/wishlist/${match.params.family}/${user_id}/edit/${gift.id}`)}>
                         <h3>{gift.name}</h3>
-                        <a>View gift at original website</a>
+
+                        {createLinkToGift(gift.gift_url)}
+
                         <p>{gift.description}</p>
                     </div>
                 ))}
