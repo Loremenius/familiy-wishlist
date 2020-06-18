e88222cc41084ed75e6e37506b7980a9660d5c54
Marcellino Fernandez <munch2000@gmail.com>
2020-06-17 23:49:00 -0600

    removed logger as import in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 import { applyMiddleware, createStore, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import { Provider } from "react-redux";
 import { reducer as loginReducer } from './redux/reducers/LoginRegisterReducer';
 import { reducer as wishlistReducer } from './redux/reducers/WishlistReducer'
