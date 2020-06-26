858035b79bcee996a33efafb76c959ebd50be692
Marcellino Fernandez <munch2000@gmail.com>
2020-06-25 22:59:24 -0600

    added comment to PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,6 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux"
 
 const PrivateRoute = ({component: Component, user, ...rest}) => {
+    // Routes using will check if there is a user id. if there is none then it will send the user back to login.
     return (<Route {...rest} render={
         props => !!user.user_id ? (
             <Component {...props}/>
