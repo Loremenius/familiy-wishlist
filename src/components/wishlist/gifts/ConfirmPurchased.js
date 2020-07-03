16575c5f81b94f380634aafbe4718e2ea743b0e6
Marcellino Fernandez <munch2000@gmail.com>
2020-07-02 23:16:21 -0600

    added comments to confirmPurchased

--- src/components/wishlist/gifts/ConfirmPurchased.js
+++ src/components/wishlist/gifts/ConfirmPurchased.js
@@ -7,15 +7,19 @@ const ConfirmPurchased = ({ match, history, gifts, confirmPurchased }) =>{
     const [giftName, setGiftName] = useState('');
 
     useEffect(()=>{
+        // find the index of the gift id from parameters
         const index = findGiftIndex(gifts, match.params.gift_id)
+        // use the index to get the gift from state.
         const gift = gifts[index];
+        // take the name of the gift and update the giftName variable for the component.
         setGiftName(gift.name);
     },[]);
 
     function confirm(){
+        // send request to back end to tag gift as purchased.
         confirmPurchased(match.params.id, match.params.gift_id, match.params.family, history);
     }
-
+    // Creates component to verify the user wants to mark a gift as purchased.
     return (
         <div className = "confirmPurchase">
             <h2>Mark as Purchased</h2>
